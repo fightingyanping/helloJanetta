e869b6a7b0f0854d6553406b59d7137356bebced
Janetta Yang <fightingyanping@163.com>
2018-07-17 09:38:21 +0800

    no add, commit directly after modifying

--- src/helloJanetta.java
+++ src/helloJanetta.java
@@ -4,7 +4,7 @@ public class helloJanetta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("use cmd to commit");
+		System.out.println("no add, commit directly after modifying");
 	}
 
 }
