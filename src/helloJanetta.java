c3ee1d80e5e0212b8ea155f5e7845dc6ca45bb32
Janetta Yang <fightingyanping@163.com>
2018-07-17 11:05:37 +0800

    v0.3

--- src/helloJanetta.java
+++ src/helloJanetta.java
@@ -4,7 +4,7 @@ public class helloJanetta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("create a new branch and commit to remote");
+		System.out.println("create tag v0.3");
 	}
 
 }
